aca2e8110e31549f722e037a0387cf167fd1526d
MaCB20 <marloncbazan@outlook.com>
2022-09-01 01:27:09 -0500

    Prev T1-2

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,15 +7,13 @@ public class PlayerController : MonoBehaviour
     Rigidbody2D rb;
     SpriteRenderer sr;
     Animator an;
-    public float Horizontal;
-    public float velocity = 5f;
-    public float jumpForce;
-    bool jump = true;
-    bool atacar = true;
+    public float velocity = 5;
+    public float jumpForce = 5;
+    bool Jump = true;
     const int A_Quieto = 0;
     const int A_Caminar = 1;
-    const int A_Saltar = 2;
-    const int A_Correr = 3;
+    const int A_Correr = 2;
+    const int A_Saltar = 3;
     const int A_Atacar = 4;
     void Start()
     {
@@ -27,50 +25,49 @@ public class PlayerController : MonoBehaviour
     }
     void Update()
     {
-        Horizontal = Input.GetAxisRaw("Horizontal") * velocity;
-        ChangeAnimation(A_Quieto);
-
-        if(Horizontal > 0.0f)
+        if(Input.GetKeyDown(KeyCode.Space) && Jump)
         {
+            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            Jump = false;
+            ChangeAnimation(A_Saltar);
+        }
+        else if(Input.GetKey(KeyCode.RightArrow) && Input.GetKey(KeyCode.X))
+        {
+            rb.velocity = new Vector2(velocity * 2, rb.velocity.y);
+            sr.flipX = false;
+            ChangeAnimation(A_Correr);
+        }
+        else if(Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.velocity = new Vector2(velocity, rb.velocity.y);
             sr.flipX = false;
             ChangeAnimation(A_Caminar);
-        }   
-        else if(Horizontal < 0.0f)
+        }
+        else if(Input.GetKey(KeyCode.LeftArrow) && Input.GetKey(KeyCode.X))
         {
+            rb.velocity = new Vector2(-velocity * 2, rb.velocity.y);
+            sr.flipX = true;
+            ChangeAnimation(A_Correr);
+        }
+        else if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.velocity = new Vector2(-velocity, rb.velocity.y);
             sr.flipX = true;
             ChangeAnimation(A_Caminar);
         }
-
-        if(Input.GetKeyDown(KeyCode.Space) && jump)
+        else if(Input.GetKeyDown(KeyCode.Z))
         {
-            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
-            jump = false;
-            ChangeAnimation(A_Saltar);
-        }
-
-        if(Input.GetKeyDown(KeyCode.Z)){
-            atacar = false;
             ChangeAnimation(A_Atacar);
         }
-
-        if(Input.GetKeyDown(KeyCode.X))
+        else
         {
-            velocity = 15f;
-            ChangeAnimation(A_Correr);
-
-        } else {
-            velocity = 5f;
+            rb.velocity = new Vector2(0, rb.velocity.y);
+            ChangeAnimation(A_Quieto);
         }
-        
     }
     void OnCollisionEnter2D(Collision2D other)
     {
-        jump = true;
-        atacar = true;
-    }
-    void FixedUpdate()
-    {
-        rb.velocity = new Vector2(Horizontal, rb.velocity.y);
+        Jump = true;
     }
     void ChangeAnimation(int animation)
     {
